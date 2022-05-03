f7db25bcd9f1e8ebe41356cac9195a7d9d7aac65
21P31A1213 <98025298+21P31A1213@users.noreply.github.com>
2022-05-03 21:45:39 +0530

    Number=int(input()) length=len(str(Number))
    Temp=Number
    Sum=0
    rem=0
    
    while Temp>0:
        rem=Temp%10
        Sum=Sum+int(rem**length)
        Temp=Temp//10
        length=length-1
    if Sum==Number:
        print('True')
    else:
        print('False')

--- Disarium_number.py
+++ Disarium_number.py
@@ -2,15 +2,15 @@ Number=int(input())
 length=len(str(Number))
 
 Temp=Number
-sum=0
+Sum=0
 rem=0
 
 while Temp>0:
     rem=Temp%10
-    sum=sum+int(rem**length)
+    Sum=Sum+int(rem**length)
     Temp=Temp//10
     length=length-1
-if sum==Number:
+if Sum==Number:
     print('True')
 else:
     print('False')
