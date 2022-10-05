a4302f848c28d43d8a417ebf998e5708271506f4
21P31A1213 <98025298+21P31A1213@users.noreply.github.com>
2022-10-05 13:00:23 +0530

    n=str(input()) c='aeiouAEOIU' b=list(n) d=[] e=[] for i in b:     if i in c:         d.append(i) for i in d:     if i not in e:         e.append(i) if len(e)>0:     print(*e) else:     print('-1')

---
+++ vowels_in__string.py
@@ -0,0 +1,16 @@
+n=str(input())
+c='aeiouAEOIU'
+b=list(n)
+d=[]
+e=[]
+for i in b:
+    if i in c:
+        d.append(i)
+for i in d:
+    if i not in e:
+        e.append(i)
+if len(e)>0:
+    print(*e)
+else:
+    print('-1')
+        
