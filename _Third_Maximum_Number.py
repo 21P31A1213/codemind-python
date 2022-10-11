1009e7836c8b6be2269b37a1627bfdc13f24aa5e
21P31A1213 <98025298+21P31A1213@users.noreply.github.com>
2022-10-11 21:57:52 +0530

    a=int(input()) arr=list(map(int,input().split())) arr.sort() dis=[] for i in arr:     if i in arr:         if i not in dis:             dis.append(i) if len(dis)<3:     print(max(dis)) else:     print(dis[len(dis)-3])

---
+++ _Third_Maximum_Number.py
@@ -0,0 +1,12 @@
+a=int(input())
+arr=list(map(int,input().split()))
+arr.sort()
+dis=[]
+for i in arr:
+    if i in arr:
+        if i not in dis:
+            dis.append(i)
+if len(dis)<3:
+    print(max(dis))
+else:
+    print(dis[len(dis)-3])
